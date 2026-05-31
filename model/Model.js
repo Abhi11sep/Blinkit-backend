fed0b5d37c2945c81b0c0ef44e95c72abb30656c
Abhi11sep <abhi.rocks.11.9.1997@gmail.com>
2026-05-31 18:32:44 +0530

    model updated

--- model/Model.js
+++ model/Model.js
@@ -1,9 +1,11 @@
 const mongoose=require('mongoose')
 
 const productSchema=new mongoose.Schema({
+    src:String,
     title:String,
     qty:String,
-    price:Number
+    price:Number,
+    count:Number,
 })
 
 const productModel=mongoose.model("products",productSchema)
